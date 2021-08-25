38c24cc6064d25018f56812154b3b16fb7465bf3
magus <harvestashya@gmail.com>
2021-08-24 17:53:46 -0700

    backend testing with aws

--- Backend/app.py
+++ Backend/app.py
@@ -3,10 +3,11 @@
 from flask import Flask, request, jsonify
 from models import db, connect_db, Image
 import os
-# from dotenv import load_dotenv
+import boto3
+from dotenv import load_dotenv
 
-# load_dotenv()
-
+load_dotenv()
+s3 = boto3.resource('s3')
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pixly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -14,6 +15,7 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
 
 # app.config['SECRET_KEY'] = os.environ.get(".env")
 app.config['SECRET_KEY'] = "This is a secret key"
@@ -36,6 +38,19 @@ def get_image(id):
 
     return jsonify(image.serialize()), 200
 
+@app.route('/test', methods=["GET"])
+def test():
+    """return the details of a single image"""
+    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",s3)
+    # breakpoint()
+    bucket = "ivan.pix.ly"
+    key = "oie_24201227QN5XVlNc.jpg"
+    url = f'https://s3.us-west-1.amazonaws.com/{bucket}/{key}'
+    # print(s3.buckets["ivan.pix.ly"])
+    for bucket in s3.buckets.all():
+        print(bucket.name)
+
+    return f"""<img src={url} alt='fdjkh'/>"""
 
 @app.route('/images', methods=["POST"])
 def add_image():
