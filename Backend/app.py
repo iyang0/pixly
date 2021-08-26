9c6315f858733e688b54b0e1c4dcf7fb4e2fa196
magus <harvestashya@gmail.com>
2021-08-26 10:02:10 -0700

    seed and basic backend changes

--- Backend/app.py
+++ Backend/app.py
@@ -8,9 +8,6 @@ import base64
 import io
 import os
 import boto3
-from dotenv import load_dotenv
-
-load_dotenv()
 
 s3 = boto3.resource('s3')
 app = Flask(__name__)
--- Backend/seed.py
+++ Backend/seed.py
@@ -1,13 +1,9 @@
 from app import app
 from models import db, Image
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
+import boto3
 
 BUCKET_NAME = os.getenv('BUCKET_NAME')
-
 
 # Create all tables
 db.drop_all()
@@ -15,11 +11,20 @@ db.create_all()
 
 # Create test data
 
-cat1 = Image(title="cat1", path="https://i.imgur.com/x2Xf5nj.jpeg", filename="x2Xf5nj")
-cat2 = Image(title="cat2", path="https://i.imgur.com/O6SqEc1.jpeg", filename="O6SqEc1")
+s3 = boto3.resource('s3')
+my_bucket = s3.Bucket(BUCKET_NAME)
+images = []
+for bucket_object in my_bucket.objects.all():
+    images.append( Image( 
+        title=bucket_object.key,
+        path=f"https://s3.us-west-1.amazonaws.com/{BUCKET_NAME}/{bucket_object.key}",
+        filename=bucket_object.key
+    ))
 
-# test1 = Image(title="space", path=f"https://s3.us-west-1.amazonaws.com/{BUCKET_NAME}/hubble-space.jpeg", filename="hubble-space.jpeg")
-# test2 = Image(title="fox", path=f"https://s3.us-west-1.amazonaws.com/{BUCKET_NAME}/howling-red-OU2vFQCwCD0-unsplash.jpg", filename="howling-red-OU2vFQCwCD0-unsplash.jpg")
+# print(images)
 
-db.session.add_all([cat1, cat2])
+# cat1 = Image(title="cat1", path="https://i.imgur.com/x2Xf5nj.jpeg", filename="x2Xf5nj.jpeg")
+# cat2 = Image(title="cat2", path="https://i.imgur.com/O6SqEc1.jpeg", filename="O6SqEc1.jpeg")
+
+db.session.add_all(images)
 db.session.commit()
