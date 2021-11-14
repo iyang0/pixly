5889227775a8bb5e2026707c0a7e1ad394bf79c5
magus <harvestashya@gmail.com>
2021-11-13 16:01:20 -0800

    start working on the ability to change things with the image

--- frontend/src/EditImageForm.js
+++ frontend/src/EditImageForm.js
@@ -1,5 +1,33 @@
-function EditImageForm(){
-  return (<form>abc</form>)
+import { Button, ButtonGroup } from 'reactstrap'
+
+function EditImageForm({ updateImage }){
+  const options = { 
+    default: 'default',
+    bw:'black-and-white',
+    sepia: 'sepia',
+    resize: 'resize',
+    border: 'border'};
+
+  function handleClick(evt) {
+    evt.preventDefault();
+    // console.log(evt.target.id);
+    updateImage(evt.target.id);
+  }
+
+  return (
+    <ButtonGroup>
+      {Object.keys(options).map( ele => (
+        <Button 
+          color='primary'
+          onClick={handleClick}
+          id={options[ele]}
+          key={ele}
+        >
+          {options[ele]}
+        </Button>
+      ))}
+    </ButtonGroup>
+    )
 }
 
 export default EditImageForm;
--- frontend/src/ImageDetails.js
+++ frontend/src/ImageDetails.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import ImageCard from './ImageCard';
 import PixlyApi from "./api";
 import { useState, useEffect } from "react";
+import EditImageForm from "./EditImageForm";
 
 /**
  * get individual image from backend and display it with an image card
@@ -18,6 +19,7 @@ function ImageDetails(){
   const { id } = useParams();
   const [isLoading, setIsLoading] = useState(true);
   const [image, setImage] = useState({});
+  const [imageOption, setImageOption] = useState('default');
 
   useEffect(function getImageOnMount() {
     async function getImage() {
@@ -28,11 +30,26 @@ function ImageDetails(){
     getImage();
   }, [id])
 
+  function changeImage(option){
+    console.log("in change Image");
+    setImageOption(option);
+  }
+
+  useEffect(function changeImageOnOptionChange(){
+    async function changeImage() {
+      let imageFromAPI = await PixlyApi.updateImage(id);
+      setImage(imageFromAPI)
+      setIsLoading(false);
+    }
+    changeImage();
+  }, [id, imageOption])
+
   if (isLoading) return <div>Is Loading...</div>;
 
   return (
   <div className="container">
     <ImageCard image={image}>
+      <EditImageForm updateImage={changeImage}/>
       <ul>
         {Object.keys(image).map( key =>{
           if(image[key]){
